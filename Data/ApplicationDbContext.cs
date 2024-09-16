eca0b337ae3c56fcea68d9d0d5c4644ed05ee984
Motjiang <motjiangbrilliant@gmail.com>
2024-09-17 00:50:27 +0200

    Added Application DbContext

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TheRead_BlogPost_API.Models.Domains;
+
+namespace TheRead_BlogPost_API.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<BlogPost> BlogPosts { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
