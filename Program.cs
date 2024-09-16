d932079c7ea3affa75415f14bde60e2e2fce17ed
Motjiang <motjiangbrilliant@gmail.com>
2024-09-17 01:00:28 +0200

    Category Interface, with 3 methods. Create, IEnumerable, GetById

--- Program.cs
+++ Program.cs
@@ -18,6 +18,8 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
 
 });
 
+
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
---
+++ Repositories/Interface/ICategoryRepository.cs
@@ -0,0 +1,11 @@
+﻿using TheRead_BlogPost_API.Models.Domains;
+
+namespace TheRead_BlogPost_API.Repositories.Interface
+{
+    public interface ICategoryRepository
+    {
+        Task<Category> CreateAsync(Category category);
+        Task<IEnumerable<Category>> GetAllAsync();
+        Task<Category?> GetByIdAsync(Guid id);
+    }
+}
