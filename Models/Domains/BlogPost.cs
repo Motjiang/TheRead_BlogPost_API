a064a03604fe5c19aa0f5ed1368af53bfc51e1dc
Motjiang <motjiangbrilliant@gmail.com>
2024-09-17 00:47:15 +0200

    Added Request Dtos

---
+++ Models/Domains/BlogPost.cs
@@ -0,0 +1,15 @@
+﻿namespace TheRead_BlogPost_API.Models.Domains
+{
+    public class BlogPost
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string ShortDescription { get; set; }
+        public string Content { get; set; }
+        public string FeaturedImageUrl { get; set; }
+        public string UrlHandle { get; set; }
+        public DateTime PublishDate { get; set; }
+        public string Author { get; set; }
+        public bool IsVisible { get; set; }
+    }
+}
