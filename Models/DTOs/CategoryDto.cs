ef7bae8724b16d7e6466337231cd0442cb0731a0
Motjiang <motjiangbrilliant@gmail.com>
2024-09-17 00:19:48 +0200

    Added the Category Model and the Data Transfer Object

---
+++ Models/DTOs/CategoryDto.cs
@@ -0,0 +1,9 @@
+﻿namespace TheRead_BlogPost_API.Models.DTOs
+{
+    public class CategoryDto
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string UrlHandle { get; set; }
+    }
+}
---
+++ Models/Domains/Category.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TheRead_BlogPost_API.Models.Domains
+{
+    public class Category
+    {
+        [Key]
+        public Guid Id { get; set; }
+        [StringLength(150)]
+        public string Name { get; set; }
+        public string UrlHandle { get; set; }
+
+    }
+}
