fde7f10269e82dcab4fdc7602e2c329cd71ad83b
Motjiang <motjiangbrilliant@gmail.com>
2024-09-17 02:07:08 +0200

    Update Request Dto

---
+++ Models/DTOs/UpdateCategoryRequestDto.cs
@@ -0,0 +1,9 @@
+﻿namespace TheRead_BlogPost_API.Models.DTOs
+{
+    public class UpdateCategoryRequestDto
+    {
+        public string Name { get; set; }
+        public string UrlHandle { get; set; }
+
+    }
+}
